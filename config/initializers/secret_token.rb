6f11c91fa2f806644cce251f532f5e71e123a4ea
Luis Vega <luiscvega@gmail.com>
2012-01-05 14:02:43 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bloomcaps::Application.config.secret_token = '1ae8be11531d4711b50dd38e643bbb48c8cda8cee8d12134fd4b52364300ef033fab79d5c07e646efa7fe5824081d28cac5fac60e37dc0056618d812a47b5ed5'
